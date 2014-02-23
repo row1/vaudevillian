cc1e5a14a15168ab79d0e67d371a6473d50be270
row1 <mcphd101@hotmail.com>
2014-02-23 22:51:04 +0800

    More work on the review template

--- src/site/views/review/tmpl/list.php
+++ src/site/views/review/tmpl/list.php
@@ -1,5 +1,15 @@
 <?php defined( '_JEXEC' ) or die( 'Restricted access' );?>
-
-<?php echo $this->_addReviewView; ?>
-
-LIST TEMPLATE
+<div class="row">
+	<div class="span12">
+	      <?php if(1 == 1 ): ?>
+	        <a href="#vv-review-modal" role="button" data-toggle="modal" class="btn pull-right"><i class="icon icon-star"></i> <?php echo JText::_('COM_VAUDEVILLIAN_ADD_REVIEW'); ?></a>
+	        <?php echo $this->_addReviewView->render(); ?>
+	      <?php endif; ?>
+	</div>
+</div>
+<div class="row">
+	<div class="span12">
+	      <h2><?php echo JText::_('COM_VAUDEVILLIAN_REVIEWS'); ?></h2>
+	      LIST TEMPLATE		
+	</div>
+</div>		
